cf3f3e2c5be160c685c8e4c936bde27697d9b173
Dikshant Koriwar <dikshantkoriwar29@gmail.com>
2023-12-24 08:39:29 +0530

    intial commit

---
+++ PracticeSet3/3.js
@@ -0,0 +1,6 @@
+User
+let correctNumber =4;
+let i=0;
+while(i !=correctNumber){
+ i= prompt("Enter a number");
+}
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+let correctNumber =4;
+// let i=0;
+// while(i !=correctNumber){
+//  i= prompt("Enter a number");
+// }
+// const readline = require('readline');
+
+// const correctNumber = 4;
+// let i = 0;
